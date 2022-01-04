2ecc72c4eeb451750f5be7705cbbc82725cc3b34
haeun <gkdms1120s@gmail.com>
2022-01-04 23:51:27 +0900

    0104

---
+++ 입출력/Q1000.java
@@ -0,0 +1,12 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class Q1000{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
---
+++ 입출력/Q2558.java
@@ -0,0 +1,16 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class Q2558 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+
+        Math.random();
+
+        in.close();
+    }
+}
